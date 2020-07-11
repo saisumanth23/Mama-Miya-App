fd590b9c4a06b7599f904668592b383a1e6cce46
avinashavin169 <34287377+avinashavin169@users.noreply.github.com>
2020-07-10 21:38:29 -0500

    font changed

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class Welcome extends Component {
     render() {
         return(
-            <h>I am Class Based mama miya</h>
+            <h>I am Class Based mama miya Component</h>
         )
     }
 }

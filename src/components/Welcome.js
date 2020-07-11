9cd63a3b0f4b602e8e1b523bc7e8998bbfe5d313
avinashavin169 <34287377+avinashavin169@users.noreply.github.com>
2020-07-10 21:12:53 -0500

    first commit

---
+++ src/components/Welcome.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Welcome extends Component {
+    render() {
+        return(
+            <h>I am Class Based mama miya</h>
+        )
+    }
+}
+
+export default Welcome;
